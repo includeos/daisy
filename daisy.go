3cd70e4fd39c2221816b4814e9c7654cfb79e355
Martin Nordsletten <mnordsletten@gmail.com>
2018-07-13 10:06:35 +0200

    Instance struct: All info about an instance is kept in this one struct

--- daisy.go
+++ daisy.go
@@ -3,9 +3,21 @@ package main
 import "fmt"
 
 const (
-	instanceCount = 3
-	inPort        = 10000
+	instanceCount      = 4
+	inPort             = 10000
+	destinationAddress = "10.100.0.20"
+	destinationPort    = 8080
 )
+
+type instance struct {
+	count          int
+	name           string
+	leftNet        subnet
+	rightNet       subnet
+	port           int
+	nextHopAddress string
+	nextHopPort    int
+}
 
 func main() {
 	// TODO: Read config file
@@ -21,41 +33,62 @@ func main() {
 		netmask: "255.255.255.128",
 		right:   "10.100.0.41",
 	}
-	// TODO: Process last destination and port
 
 	// Create intermediate subnets
 	subnets := []subnet{first}
 	subnets = append(subnets, generateSubnets(instanceCount)...)
 	subnets = append(subnets, last)
 
-	printLayout(subnets)
-	generateNacls(subnets)
+	instances := createInstances(subnets)
+	printInstances(instances)
+	generateNacls(instances)
 
 	// Fill in template with subnet info
 
 }
 
-func printLayout(nets []subnet) {
-	fmt.Printf("%-15s %-6s %-4s %-15s\n", "left", "port", "iOS", "right")
-	for i := 0; i < instanceCount; i++ {
-		fmt.Printf("%-15s %-6d %-4d %-15s\n", nets[i].left, inPort+i+1, i+1, nets[i+1].right)
-	}
-}
-
-func generateNacls(nets []subnet) {
-	for i := 0; i < instanceCount; i++ {
+func generateNacls(instances []instance) {
+	for _, in := range instances {
 		d := daisyTemplate{
-			GwLeftNet:      nets[i].net,
-			GwLeftNetmask:  nets[i].netmask,
-			LeftAddress:    nets[i].left,
-			GwRightNet:     nets[i+1].net,
-			GwRightNetmask: nets[i+1].netmask,
-			RightAddress:   nets[i+1].right,
-			LeftPort:       inPort + i + 1,
-			NextHopAddress: nets[i+1].left,
-			NextHopPort:    inPort + i + 2,
+			GwLeftNet:      in.leftNet.net,
+			GwLeftNetmask:  in.leftNet.netmask,
+			LeftAddress:    in.leftNet.left,
+			GwRightNet:     in.rightNet.net,
+			GwRightNetmask: in.rightNet.netmask,
+			RightAddress:   in.rightNet.right,
+			LeftPort:       in.port,
+			NextHopAddress: in.nextHopAddress,
+			NextHopPort:    in.nextHopPort,
 		}
 		parse(d)
 		fmt.Println("################################################")
+	}
+}
+
+func createInstances(nets []subnet) []instance {
+	var instances []instance
+	for i := 0; i < instanceCount; i++ {
+		in := instance{
+			count:          i + 1,
+			name:           "name",
+			leftNet:        nets[i],
+			rightNet:       nets[i+1],
+			port:           inPort + i + 1,
+			nextHopAddress: nets[i+1].left,
+			nextHopPort:    inPort + i + 2,
+		}
+		if i == instanceCount-1 {
+			in.nextHopAddress = destinationAddress
+			in.nextHopPort = destinationPort
+		}
+		instances = append(instances, in)
+	}
+	return instances
+}
+
+func printInstances(in []instance) {
+	fmt.Printf("%-15s %-6s %-4s %-15s %s\n", "left", "port", "iOS", "right", "nextHop")
+	for _, x := range in {
+		fmt.Printf("%-15s %-6d %-4d %-15s %s:%d\n", x.leftNet.left, x.port, x.count, x.rightNet.right, x.nextHopAddress, x.nextHopPort)
 	}
 }
